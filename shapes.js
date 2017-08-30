a56ec5a5dcefac34bda5eb709ed2b0c3a3a20abd
yogashri1126 <ypradhan.pge@texas.edu>
2017-08-30 05:25:35 -0500

    I was able to complete all the steps except the constructor changing to get triangle to report to Triangle. The only way I did it was brute force to change the prototype to new Shape(actual shape constructor). I tried doing a different variable to get new Triangle() but I keep getting undefined or Function again

--- shapes.js
+++ shapes.js
@@ -1,25 +1,29 @@
 //Step 1
 
-var shape = new Shape(Triangle)
-
 function Shape(get_type){
+	//this.type= type;
 	this.get_type=function(){
 
-	Object.getPrototypeof(this)
+	return this.constructor
+	//Object.getPrototypeof(this)
+	console.log(this.constructor)
 	}
-	console.log("Step 4 below:")
-	console.log(get_type)
+	//console.log("Step 4 below:")
 	
 	
 	return get_type
-	
+	console.log(get_type)
 }
+
+var shape = new Shape()
 
 console.log("When I do Shape.get_type:")
 console.log(shape.get_type)
+console.log(" ")
 //Question 4
 //Whenever I do "Shape.get_type I get undefined. I think this happens because for a 
 //constructor function, I would not be able to use .getPrototypeOf. I instead did Shape.prototype.get_type
+
 
 Shape.prototype.get_type = function(){
     return this.constructor;
@@ -27,6 +31,7 @@ Shape.prototype.get_type = function(){
 }
 
 console.log(Shape.prototype.get_type)
+console.log(" ")
 
 //Steps 2 & 3
 
@@ -36,11 +41,33 @@ function Triangle(side1, side2, side3){
 	this.side1= side1
 	this.side2= side2
 	this.side3= side3
-
+	//Shape.call(this, arguments)
 }
+
+console.log("triangle.prototype:")
+console.log(triangle.prototype= new Shape())
+console.log(" ")
+
+console.log("Proof the constructor is working")
+console.log(Shape(triangle))
+console.log(" ")
+
 console.log("triangle:")
 console.log(triangle)
+console.log(" ")
+Triangle.prototype= new Shape()
 
+//Reporting the instance of a triangle to Triangle
+triangle.prototype= new Shape(Triangle)
+
+//I'm having trouble getting this to return the type of function being Triangle instead of just Function
+triangle.get_type = function(){
+
+    return this.constructor;
+    console.log(this.constructor)
+}
+
+console.log(triangle.get_type)
 
 var square = new Square(5,5,5,5)
 
@@ -55,6 +82,12 @@ function Square(side1, side2, side3, side4)
 
 console.log("square")
 console.log(square)
+console.log(" ")
+Square.prototype= new Shape()
+
+//Reporting the instance of a square to Square
+square.prototype= new Shape(Square)
+console.log(square.prototype)
 
 var pentagon= new Pentagon(5,5,5,5,5)
 function Pentagon(side1, side2, side3, side4, side5)
@@ -69,36 +102,60 @@ function Pentagon(side1, side2, side3, side4, side5)
 
 console.log("pentagon")
 console.log(pentagon)
+console.log(" ")
+Pentagon.prototype= new Shape()
 
+//Reporting the instance of a pentagon to Pentagon
+pentagon.prototype= new Shape(Pentagon)
 //Step 4
 
 homework= Object.setPrototypeOf(triangle, Shape)
 homework2= Object.setPrototypeOf(square, Shape)
 homework3= Object.setPrototypeOf(pentagon, Shape)
-test=Object.getPrototypeOf(triangle)
+test= Object.getPrototypeOf(triangle)
 testing= Object.getPrototypeOf(square)
 testaholic= Object.getPrototypeOf(pentagon)
-question4=Object.getPrototypeOf(Shape)
+question4= Object.getPrototypeOf(Shape)
 
-console.log(shape.prototype.constructor)
+Shape.call(triangle, get_type)
+Shape.call(square, get_type)
+Shape.call(pentagon, get_type)
+
+Triangle.call(triangle, arguments)
+Square.call(square,arguments)
+Pentagon.call(pentagon, arguments)
+
+//call function works here!
+console.log(triangle.get_type())
+console.log(" ")
+//console.log(shape.prototype.constructor)
 
 
-console.log("Set prototype of triangle:")
-console.log(homework)
-
+// console.log("Set prototype of triangle:")
+// console.log(homework)
+// console.log(" ")
+console.log("checking instance of triangle as a part of the Triangle constructor")
+console.log(Shape instanceof Triangle)
+console.log(" ")
 console.log("triangle get type:")
-console.log(triangle.get_type)
+console.log(triangle.get_type())
+//console.log(triangle.prototype.get_type())
 
-console.log("Set prototype of square:")
-console.log(homework2)
-console.log("Set prototype of pentagon:")
-console.log(homework3)
+// console.log("Set prototype of square:")
+// console.log(homework2)
+// console.log(" ")
+// console.log("Set prototype of pentagon:")
+// console.log(homework3)
+// console.log(" ")
 console.log("Get prototype of triangle:")
-console.log(test)
+console.log(triangle.prototype)
+console.log(" ")
 console.log("Get prototype of square:")
-console.log(testing)
+console.log(square.prototype)
+console.log(" ")
 console.log("Get prototype of pentagon:")
-console.log(testaholic)
+console.log(pentagon.prototype)
+console.log(" ")
 
 //get type shape
 console.log(question4)
